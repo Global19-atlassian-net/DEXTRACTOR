b679bf1da932b2a7f0073dfd5040ca9864736333
Christopher Dunn <cdunn@pacificbiosciences.com>
2018-05-22 12:01:12 -0700

    Copied from DAMASKER

---
+++ bamboo_build.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -e
+type module >& /dev/null || source /mnt/software/Modules/current/init/bash
+
+module load gcc
+module load git
+module load meson
+module load ninja
+module load ccache
+
+set -vex
+
+export CCACHE_COMPILERCHECK='%compiler% -dumpversion'
+
+case "${bamboo_planRepository_branchName}" in
+  develop|master)
+    module load dazzdb/${bamboo_planRepository_branchName}
+    export PREFIX_ARG="/mnt/software/d/dextractor/${bamboo_planRepository_branchName}"
+    export BUILD_NUMBER="${bamboo_globalBuildNumber:-0}"
+    ;;
+  *)
+    module load dazzdb/develop
+    export PREFIX_ARG=/PREFIX
+    export BUILD_NUMBER="0"
+    ;;
+esac
+
+# rm -rf ./build
+meson --buildtype=release --strip --libdir=lib --prefix="${PREFIX_ARG}" -Dtests=false --wrap-mode nofallback ./build .
+
+TERM='dumb' ninja -C ./build -v
+
+DESTDIR="$(pwd)/DESTDIR"
+rm -rf "${DESTDIR}"
+TERM='dumb' DESTDIR="${DESTDIR}" ninja -C ./build -v install
+
+rm -rf "${DESTDIR}"
+
+case "${bamboo_planRepository_branchName}" in
+  develop|master)
+    DESTDIR=
+    TERM='dumb' DESTDIR="${DESTDIR}" ninja -C ./build -v install
+    chmod -R a+rwx "${DESTDIR}${PREFIX_ARG}"/*
+    module load dextractor/${bamboo_planRepository_branchName}
+    ldd -r $(which TANmask)
+    ;;
+esac
